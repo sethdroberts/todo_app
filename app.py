ad31b7ee999e9a63956ab45fdcf9a52d104be3b6
Seth Roberts <sethknock@gmail.com>
2025-05-05 03:07:46 +0000

    Build out basic layouts

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+
+# poetry run python app.py
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>This is a todo starter!</h1>"
+    return redirect(url_for('get_lists'))
+    
+@app.route('/lists')
+def get_lists():
+        
+    lists = [
+    {"title": "Lunch Groceries", "todos": []},
+    {"title": "Dinner Groceries", "todos": []},
+    ]
+    
+    return render_template('lists.html', lists=lists)
+    
+@app.route('/lists/new')
+def add_todo_list():
+    return render_template('new_list.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5003)
+    app.run(debug=True, port=8080)
